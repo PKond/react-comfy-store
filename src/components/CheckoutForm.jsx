b739dd100f2b5dd78fc607556c0a8b4f00ef3310
Pandelis <pandelis777@gmail.com>
2024-10-30 22:52:34 +0200

    After Video 505 Checkout Page- Setup

---
+++ src/components/CheckoutForm.jsx
@@ -0,0 +1,6 @@
+const CheckoutForm = () => {
+  return (
+	<h1 className="text-3xl">CheckoutForm</h1>
+  )
+}
+export default CheckoutForm;
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,20 @@
+import { useSelector } from "react-redux";
+import { CheckoutForm, SectionTitle, CartTotals } from "../components";
+
 const Checkout = () => {
+  const cartTotal = useSelector((state) => state.cartState.cartTotal);
+  console.log(cartTotal);
+  if (cartTotal === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
   return (
-	<h1 className="text-4xl">Checkout</h1>
-  )
-}
-export default Checkout
+    <>
+      <SectionTitle text="Place your order" />
+	  <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+		<CheckoutForm />
+		<CartTotals />
+	  </div>
+    </>
+  );
+};
+export default Checkout;
